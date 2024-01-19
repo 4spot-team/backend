c3f09f4bd4215b281430f3a13e6a50c7a0e5de93
Giovanni <gpettinacci5@gmail.com>
2024-01-19 18:51:39 +0100

    Add 'success' field to terms responses

--- src/controllers/terms.js
+++ src/controllers/terms.js
@@ -9,24 +9,34 @@ async function acceptTerms(req, res) {
         const user = await Stakeholder.findOne({ username });
 
         if(!user) {
-            return res.status(400).json({ message: "User not found" });
+            return res.status(400).json({
+                success: false,
+                message: "User not found" 
+            });
         }
 
         if (user.hasAcceptedTerms) {
-            return res.status(400)
-                      .json({ message: "Acceptance already given" });
+            return res.status(400).json({ 
+                success: false,
+                message: "Acceptance already given" 
+            });
         }
 
         user.hasAcceptedTerms = true;
 
         await user.save();
 
-        res.status(200)
-           .json({ message: "Acceptance given successfully" });
+        res.status(200).json({ 
+            success: true,
+            message: "Acceptance given successfully" 
+        });
 
     } catch(error) {
         console.error(error);
-        res.status(500).json({ message: "Acceptance failed" });
+        res.status(500).json({ 
+            success: false,
+            message: "Acceptance failed" 
+        });
     }
 
 }
