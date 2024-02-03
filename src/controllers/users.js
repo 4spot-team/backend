f9f81f72f31f40b8fd4b8c348bac4a567e4e97d7
davideciacciolo <davide.ciacciolo@outlook.com>
2024-02-03 16:46:19 +0100

    Fix errors and check external input

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,18 @@
 const { Event } = require("../models/event");
 const { Stakeholder } = require("../models/stakeholder");
 
+async function populateEventFields(item, index, arr) {
+    await arr[index]
+        .populate('organiser')
+        .populate('tickets')
+        .populate({
+            path: 'types',
+            populate: { path: 'superType' },
+        })
+        // .populate('messagingGroup')
+        .exec();
+}
+
 
 // GET '/:username'
 async function getUserPage(req, res) {
@@ -8,17 +20,28 @@ async function getUserPage(req, res) {
         const { username } = req.params;
 
         const user = await Stakeholder.findOne({ username });
-        const following = user.following;
-        const followers = user.followers;
+
+        if (typeof user === "undefined") {
+            return res.status(400).json({
+                success:false,
+                message: "Could not find required user",
+            });
+        }
 
         const events = await Event.find({ organiser: user });
+
+        await user
+            .populate('followers', 'username')
+            .populate('following', 'username')
+            .exec();
+
+        // TODO probably is not necessary to populate all the Event fields
+        await events.forEach(populateEventFields);
 
         const response = {
             success: true,
             message: 'User page retrieved successfully',
             user: user,
-            following: following,
-            followers: followers,
             events: events,
         };
 
@@ -33,45 +56,64 @@ async function getUserPage(req, res) {
 // POST '/:username'
 async function postUserPage(req, res) {
     try {
-        const { currentUsername } = req.body;
-        const { paramUsername } = req.params;
 
-        const currentUser = await Stakeholder.findOne(currentUsername);
+        // Provided by checkToken middleware function
+        const { username } = req;
+        const paramUsername = req.params.username;
+
+        const user = await Stakeholder.findOne(username);
         const paramUser = await Stakeholder.findOne(paramUsername);
 
-        if currentUser != paramUser {
+        if (typeof paramUser === "undefined") {
+            return res.status(400).json({
+                success:false,
+                message:"Could not find username specified in request params",
+            });
+        }
+
+        if user != paramUser {
             // POST to other user page, othrewise no operation available
 
             const { follow } = req.body;  // boolean
-            if (follow) {
-                currentUser.following.push(paramUser);
-                paramUser.followers.push(currentUser);
+            if (typeof follow !== "undefined") {
+                if (typeof follow === "boolean") {
+                    if (follow) {
+                        currentUser.following.push(paramUser);
+                        paramUser.followers.push(currentUser);
+                    } else {
+                        const firstIndex = user.following.indexOf(paramUser);
+                        const secondIndex = paramUser.followers.indexOf(currentUser);
 
-            } else {
-                const firstIndex = currentUser.following.indexOf(paramUser);
-                const secondIndex = paramUser.followers.indexOf(currentUser);
+                        if (firstIndex > -1 && secondIndex > -1) {
 
-                if (firstIndex > -1 && secondIndex > -1) {
+                            currentUser.following.splice(firstIndex, 1);
+                            paramUser.followers.splice(secondIndex, 1);
 
-                    currentUser.following.splice(firstIndex, 1);
-                    paramUser.followers.splice(secondIndex, 1);
+                        } else {
 
-                } else {
-
-                    const response = {
-                        success: false,
-                        message: 'Target user is not in following list, target user has not current user in followers list, or both',
+                            return res.status(400).json({
+                                success: false,
+                                message: 'Target user is not in following list, target user has not current user in followers list, or both',
+                            });
+                        }
                     }
-                    // TODO check if status code is adequate
-                    return res.status(400).json(response);
+                } else {
+                    return res.status(400).json({
+                        success:false,
+                        message:"Provided 'follow' field is not a boolean",
+                    });
                 }
+            } else {
+                return res.status(400).json({
+                    success:false,
+                    message:"Required 'follow' field missing in request body",
+                });
             }
         } else {
             const response = {
                 success = false,
                 message: 'No POST for self user page',
             }
-            // TODO check if status code is adequate
             return res.status(400).json(response);
         }
         
